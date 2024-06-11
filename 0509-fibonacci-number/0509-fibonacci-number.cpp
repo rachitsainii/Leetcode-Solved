e7e04e58b061ce8f8ee011d2664a8f6edffacf03
Rachit <rachit.saini@icloud.com>
2024-06-11 14:28:22 -0700

    Time: 0 ms (100%), Space: 7.3 MB (11.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int fib(int n) {
+    // Normal Recursion
+    int solveUsingRecursion(int n){
         // Base Case (Mandatory)
         if(n == 0 || n == 1){
             return n;
@@ -9,8 +10,38 @@ public:
         // Processing (Optional)
 
         // Recursive Relation (Mandatory)
-        int ans = fib(n-1) + fib(n-2);
+        int ans = solveUsingRecursion(n-1) + solveUsingRecursion(n-2);
 
+        return ans;
+    }
+
+    // Top-down approach
+    // Recommended approach
+    int solveUsingMemoization(int n, vector<int> &dp){
+        // Base Case (Mandatory)
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        // step 3: If answer already exists then return ans
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        // Step 2: Store and return using dp array
+        dp[n] = solveUsingMemoization(n-1, dp) + solveUsingMemoization(n-2, dp);
+
+        return dp[n];
+
+    }
+
+    int fib(int n) {
+
+        // int ans = solveUsingRecursion(n);
+        // return ans;
+        // Step 1: Create a DP array
+        vector<int> dp(n+1, -1);
+        int ans = solveUsingMemoization(n, dp);
         return ans;
         
     }
