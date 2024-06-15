d4808acc15f87ee364534dc581a1f475f9e9da46
Rachit <rachit.saini@icloud.com>
2024-06-15 09:36:06 -0700

    Time: 0 ms (100%), Space: 9.3 MB (35.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -34,18 +34,31 @@ public:
     }
 
     // Tabulation
-    // int solveUsingTabulation(vector<int> &nums){
-    //     int n = nums.size();
-    //     vector<int> dp(n+1, -1);
-    // }
+    int solveUsingTabulation(vector<int> &nums){
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[n-1] = nums[n-1];
+        for(int index = n-2; index >= 0; index--){
+            int tempAns = 0;
+            if(index+2 < n){
+                tempAns = dp[index+2];
+            }
+            int include = nums[index] + tempAns;
+            int exclude = 0 + dp[index+1];
+            dp[index] = max(include, exclude);
+        }
+        return dp[0];
+    }
 
     int rob(vector<int>& nums) {
         int index = 0;
         //int ans = solveUsingRecursion(nums, index);
         //return ans;
         int n = nums.size();
-        vector<int> dp(n, -1);
-        int ans = solveUsingMem(nums, dp, index);
+        // vector<int> dp(n, -1);
+        // int ans = solveUsingMem(nums, dp, index);
+        // return ans;
+        int ans = solveUsingTabulation(nums);
         return ans;
     }
 };
