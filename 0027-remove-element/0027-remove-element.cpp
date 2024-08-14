bcfa7bc1179cee1830141a293a73d5f391cecb53
Rachit <rachit.saini@icloud.com>
2024-08-14 15:21:10 -0700

    Time: 0 ms (100%), Space: 10.7 MB (22.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size() == 0){
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int maximum = nums[nums.size()-1];
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == val){
+                nums[i] =  maximum + 1;
+                count++;
+            }
+        }
+
+        sort(nums.begin(), nums.end());
+
+        return nums.size() - count;
+    }
+};
