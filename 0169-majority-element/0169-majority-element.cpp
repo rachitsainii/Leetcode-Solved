131fdd2bd715279c1642f8ef337897ca8445fe5c
Rachit <rachit.saini@icloud.com>
2024-09-08 21:47:36 -0700

    Time: 18 ms (52.67%), Space: 28.1 MB (24.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int, int> counts;
         for(auto i : nums){
             counts[i]++;
         }
-
-        int n = nums.size();
-        int majority = 0;
-        for(auto i : counts){
-            if(counts[i.first] > (n / 2)){
-                majority = i.first;
-            } else {
-                continue;
+        int ans = 0;
+        for(auto [key, value] : counts){
+            if(counts[key] > n / 2){
+                ans = key;
             }
         }
 
-        for(auto i : counts){
-            cout << i.first << ":" << i.second << endl;
+        for(auto [key, value] : counts){
+            cout << key << " " << value << endl;
         }
 
-        return majority;
+        return ans;
     }
 };
