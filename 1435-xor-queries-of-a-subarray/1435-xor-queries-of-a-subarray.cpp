d097d800ba3ed5ccb5956c3aab557878c9725834
Rachit <rachit.saini@icloud.com>
2024-09-12 17:52:50 -0700

    Time: 72 ms (29.98%), Space: 41.6 MB (90.33%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        vector<int> ans;
+        // Brute Force
+        // vector<int> ans;
+        // for(int i = 0; i < queries.size(); i++){
+        //     int left = queries[i][0];
+        //     int right = queries[i][1];
+        //     int val = arr[left];
+        //     for(int j = left + 1; j <= right; j++){
+        //         val = val ^ arr[j];
+
+        //     }
+        //     ans.push_back(val);
+        // }
+
+        // return ans;
+
+        // Using Prefix Sum
+        int n = arr.size();
+        vector<int> prefix(n);
+        vector<int> ans(queries.size());
+
+        // Fill in the prefix_sum
+        prefix[0] = arr[0];
+        for(int i = 1; i < arr.size(); i++){
+            prefix[i] = prefix[i-1] ^ arr[i];
+        }
+
+        for(auto i : prefix){
+            cout << i << endl;
+        }
+
         for(int i = 0; i < queries.size(); i++){
             int left = queries[i][0];
             int right = queries[i][1];
-            int val = arr[left];
-            for(int j = left + 1; j <= right; j++){
-                val = val ^ arr[j];
-
+            if(left == 0){
+                ans[i] = prefix[right];
+            } else{
+                ans[i] = prefix[right] ^ prefix[left-1];
             }
-            ans.push_back(val);
-        }
+        } 
 
         return ans;
     }
