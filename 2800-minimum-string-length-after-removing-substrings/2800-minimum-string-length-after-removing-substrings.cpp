c83db879dacffbf082a2d42500a183ad049b3e8c
Rachit <rachit.saini@icloud.com>
2024-10-06 19:18:01 -0700

    Time: 4 ms (76.08%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,39 +1,63 @@
 class Solution {
 public:
     int minLength(string s) {
-        int n = s.size();
-        while(s.size() > 0){
-            for(int i = 1; i < s.size(); i++){
-            if((s[i-1] == 'A' && s[i] == 'B') || (s[i-1] == 'C' && s[i] == 'D')){
-                s[i-1]= '#';
-                s[i] = '#';
-                }
+    //     int n = s.size();
+    //     while(s.size() > 0){
+    //         for(int i = 1; i < s.size(); i++){
+    //         if((s[i-1] == 'A' && s[i] == 'B') || (s[i-1] == 'C' && s[i] == 'D')){
+    //             s[i-1]= '#';
+    //             s[i] = '#';
+    //             }
+    //         }
+
+
+    //         string new_s = "";
+
+    //         for(int i = 0; i < s.size(); i++){
+    //             if(s[i] != '#'){
+    //                 new_s += s[i];
+    //             }
+    //         }
+
+    //         if(new_s.size() == s.size()){
+    //             return s.size();
+    //         } else {
+    //             s.size() - new_s.size();
+    //             s = new_s;
+    //         }
+    //     }
+        
+
+        
+
+    //     for(auto i : s){
+    //         cout << i;
+    //     }
+
+    //     return s.size();
+
+
+    // Using Stack
+
+        stack<char> stack;
+
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i];
+
+            if(stack.empty()){
+                stack.push(c);
+                continue;
             }
 
-
-            string new_s = "";
-
-            for(int i = 0; i < s.size(); i++){
-                if(s[i] != '#'){
-                    new_s += s[i];
-                }
-            }
-
-            if(new_s.size() == s.size()){
-                return s.size();
+            if(c == 'B' && stack.top() == 'A'){
+                stack.pop();
+            } else if(c == 'D' && stack.top() == 'C'){
+                stack.pop();
             } else {
-                s.size() - new_s.size();
-                s = new_s;
+                stack.push(c);
             }
         }
-        
 
-        
-
-        for(auto i : s){
-            cout << i;
-        }
-
-        return s.size();
+        return stack.size();
     }
 };
