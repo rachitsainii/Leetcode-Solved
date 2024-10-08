ab4104163d96697370318cc4a9cb4ba7549ec7fd
Rachit <rachit.saini@icloud.com>
2024-10-07 17:44:24 -0700

    Time: 95 ms (87.14%), Space: 32.6 MB (79.33%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+       int ans = 0;
+       for(int i = 0; i < s.size(); i++){
+        if(s[i] == '['){
+            ans++;
+        } else {
+            if(ans > 0){
+                ans--;
+            }
+        }
+    }
+        return (ans +1) / 2;
+    }
+};
