b936b9a7aa81b8b67ff2430011510621ca759492
Rachit <rachit.saini@icloud.com>
2024-10-07 23:29:40 -0700

    Time: 187 ms (28.46%), Space: 84.9 MB (31.92%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> counts;
+        vector<pair<int, int>> vp;
+        pair<int, int> p;
+        for(int i = 0; i < arr.size(); i++){
+            counts[arr[i]]++;
+        }
+
+        for(auto [key, val] : counts){
+            p.first = key;
+            p.second = val;
+            vp.push_back(p);
+        }
+
+        sort(vp.begin(), vp.end(), [](const pair<int, int> p1, const pair<int, int> p2) {return  p1.second > p2.second;});
+
+        int total = arr.size();
+        int ans = 0;
+        int it = 0;
+        while(total > arr.size() / 2 && it < vp.size()){
+            ans++;
+            total -= vp[it].second;
+            cout << total << endl;
+            it++;
+        }
+
+        // for(auto i : vp){
+        //     cout << i.first << ":" << i.second << endl;
+        // }
+
+        
+
+        return ans;
+    }
+};
