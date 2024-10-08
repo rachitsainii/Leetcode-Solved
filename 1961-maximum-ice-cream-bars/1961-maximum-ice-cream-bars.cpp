b666ee8df88e94e481885ec2988168903f92c6fb
Rachit <rachit.saini@icloud.com>
2024-10-07 23:37:18 -0700

    Time: 163 ms (5.05%), Space: 80.3 MB (74.47%) - LeetHub

---
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+
+        sort(costs.begin(), costs.end());
+
+        if(costs[0] > coins){
+            return 0;
+        }
+
+        int ans = 0;
+        int it = 0;
+        while(coins > 0 && it < costs.size()){
+            
+            if(coins - costs[it] >= 0){
+                coins -= costs[it];
+            } else {
+                return ans;
+            }
+            cout<< coins << " ";
+            ans++;
+            it++;
+        }
+
+        return ans;
+    }
+};
