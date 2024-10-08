ac59b6bec5cfc780cb08c027a10d8f6a4c130061
Rachit <rachit.saini@icloud.com>
2024-10-07 23:37:26 -0700

    Time: 122 ms (74.77%), Space: 80.3 MB (74.47%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -11,13 +11,11 @@ public:
         int ans = 0;
         int it = 0;
         while(coins > 0 && it < costs.size()){
-            
             if(coins - costs[it] >= 0){
                 coins -= costs[it];
             } else {
                 return ans;
             }
-            cout<< coins << " ";
             ans++;
             it++;
         }
