9b394fd4daf6485aba9bbec5298e03acfe6f223a
Rachit <rachit.saini@icloud.com>
2024-10-15 19:44:00 -0700

    Time: 3 ms (43.32%), Space: 7.8 MB (30.3%) - LeetHub

---
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        // Priortiy Queue to store the characters
+        priority_queue<pair<int, char>> pq;
+        if(a > 0){
+            pq.push({a, 'a'});
+        }
+
+        if(b > 0){
+            pq.push({b, 'b'}) ;
+        }
+
+        if(c > 0){
+            pq.push({c, 'c'});
+        }
+
+        string result = "";
+
+
+        while(!pq.empty()){
+            // Get the character with most frequency
+            auto [count1, char1] = pq.top();
+            pq.pop();
+
+
+            // Check if the last two characters in result are the same.
+            if(result.size() >= 2 && result.back() == char1 && result[result.size()-2] == char1){
+                if(pq.empty()){
+                    break;
+                    // No valid characters left
+                }
+
+                // Get the second most character
+                auto [count2, char2] = pq.top();
+                pq.pop();
+
+                // Add the second character to avoid the repetition
+                result += char2;
+                count2--;
+
+                if(count2 > 0){
+                    pq.push({count2, char2});
+                }
+
+                // Push back the most frequent character for later use
+                pq.push({count1, char1});
+            } else {
+                // If no repetiton issue, add the most frequent character
+                result += char1;
+                count1--;
+
+                if(count1 > 0){
+                    pq.push({count1, char1});
+                }
+            }
+        }
+
+        return result;
+    }
+};
