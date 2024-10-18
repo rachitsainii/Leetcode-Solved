a3d12e4832a1627d1401d5c7136d853a2f997bc5
Rachit <rachit.saini@icloud.com>
2024-10-17 23:34:08 -0700

    Time: 0 ms (100%), Space: 17.6 MB (94.14%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,8 +16,24 @@ public:
             return 0;
         }
 
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        return max(left, right) + 1;
+        stack<pair<TreeNode*, int >> stack;
+        stack.push(make_pair(root, 1));
+        int ans = 0;
+
+        while(!stack.empty()){
+            auto [node, depth] = stack.top();
+            stack.pop();
+            ans = max(ans, depth);
+
+            if(node->left != NULL){
+                stack.push(make_pair(node->left, depth+1));
+            }
+
+            if(node->right != NULL){
+                stack.push(make_pair(node->right, depth+1));
+            }
+        }
+
+        return ans;
     }
 };
