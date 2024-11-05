da7e34cd500f42742b0979c816f54301f373b3c3
Rachit <rachit.saini@icloud.com>
2024-11-04 17:41:41 -0800

    Time: 2 ms (47.87%), Space: 12.2 MB (16.88%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int minChanges(string s) {
+        // int ans = 0;
+        // for(int i = 1; i < s.size(); i+=2){
+        //     if(s[i] == s[i-1]){
+        //         continue;
+        //     } else if(s[i] == '1' && s[i-1] == '0'){
+        //         ans++;
+        //     } else {
+        //         ans++;
+        //     }
+        // }
+
         int ans = 0;
         for(int i = 1; i < s.size(); i+=2){
             if(s[i] == s[i-1]){
                 continue;
-            } else if(s[i] == '1' && s[i-1] == '0'){
-                //s[i] = '0';
-                ans++;
             } else {
-                //s[i] = '1';
                 ans++;
             }
         }
+
 
         return ans;
     }
