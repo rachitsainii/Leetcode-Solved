c6c9ece72963d650c809914fa520681ee7151cad
Rachit <rachit.saini@icloud.com>
2024-11-30 23:29:46 -0800

    Time: 0 ms (100%), Space: 13.7 MB (16.3%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> seen;
+        for(auto i : arr){
+            if(seen.count(2 * i) || (i % 2 == 0 && seen.count(i/2))){
+                return true;
+            }
+
+            seen.insert(i);
+        }
+
+        return false;
+    }
+};
