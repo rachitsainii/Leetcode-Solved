670626cdf5a0e3a671df5c314fc6d0b08c68a505
Rachit <rachit.saini@icloud.com>
2024-12-06 22:20:04 -0800

    Time: 31 ms (74%), Space: 59.8 MB (15.54%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    bool isPossible(vector<int> &nums, int maxOps, int mid){
+        int totalOps = 0; // to bring each number <= mid;
+
+        for(auto num: nums){
+            int ops = num / mid;
+
+            if(num % mid == 0){
+                ops -= 1;
+            } 
+
+            totalOps += ops;
+        }
+
+        return totalOps <= maxOps;
+    }
+
+
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int l = 1;
+        int r = *max_element(nums.begin(), nums.end());
+
+        int result = r;
+
+        while(l <= r){
+            int mid = l + (r-l) / 2;
+
+            if(isPossible(nums, maxOperations, mid) == true){
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return result;
+    }
+};
