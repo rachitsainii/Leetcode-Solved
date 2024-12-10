1282fa6f883d3bd026f3b700d1fb4e05745939a8
Rachit <rachit.saini@icloud.com>
2024-12-10 11:47:38 -0800

    Time: 7 ms (82.8%), Space: 12.4 MB (53.98%) - LeetHub

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximumLength(string s) {
+        
+        // Brute Force
+        int n = s.size();
+        unordered_map<string, int> mp;
+
+        for(int i = 0 ; i < n; i++){
+            string curr = "";
+            for(int j = i; j < n; j++){
+                if(curr.empty() || curr.back() == s[j]){
+                    curr.push_back(s[j]);
+                    mp[curr] += 1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        int result = 0;
+        for(auto &it: mp){
+            string str = it.first;
+            int count = it.second;
+            if(count >= 3 && str.length() > result){
+                result = str.length();
+            }
+        }
+
+        return result == 0 ? -1 : result;
+
+
+    }
+};
