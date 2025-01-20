a7b9c4ab5a22aacf76fc78df8de3b8c986967d2e
Rachit <rachit.saini@icloud.com>
2025-01-19 16:53:54 -0800

    Time: 3542 ms (5.18%), Space: 171 MB (37.8%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        unordered_map<int, pair<int, int>> positions;
+
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                positions[mat[i][j]] = {i, j};
+            }
+        }
+
+        for(auto i : positions){
+            cout << i.first << "- " << "{"<< i.second.first << ", " << i.second.second << "}" << endl;
+        }
+
+        vector<int> rowCount(m, 0);
+        vector<int> colCount(n, 0);
+        for(int i = 0; i < arr.size(); i++){
+            auto [r, c] = positions[arr[i]];
+
+            if(++rowCount[r] == n){
+                return i;
+            }
+
+            if(++colCount[c] == m){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
