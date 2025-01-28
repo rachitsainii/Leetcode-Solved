8e69ccff5da2ffdf404f4650374b599fde048585
Rachit <rachit.saini@icloud.com>
2025-01-28 15:38:12 -0800

    Time: 8 ms (23.26%), Space: 31.6 MB (21.28%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    int m, n;
+    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    int bfs(int i, int j, vector<vector<int>> &grid){
+        queue<pair<int, int>> q;
+        q.push({i, j});
+
+        int area = grid[i][j];
+        grid[i][j] = 0;
+
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for(auto &dir : directions){
+                int i_ = dir[0] + i;
+                int j_ = dir[1] + j;
+
+                if(i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && grid[i_][j_] > 0){
+                    q.push({i_, j_});
+                    area += grid[i_][j_];
+                    grid[i_][j_] = 0;
+                    
+                } 
+            }
+        }
+
+        return area;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+
+        int maxArea = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] > 0){
+                    maxArea = max(maxArea, bfs(i, j, grid));
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
