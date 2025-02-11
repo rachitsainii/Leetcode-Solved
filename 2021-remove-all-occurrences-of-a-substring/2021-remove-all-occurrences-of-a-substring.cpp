bd6d120b785c98c62becd5476ea471d7e68a701b
Rachit <rachit.saini@icloud.com>
2025-02-11 11:08:43 -0800

    Time: 17 ms (5.85%), Space: 28.3 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,18 +1,59 @@
 class Solution {
 public:
+
+    bool check(stack<char> &st, string part, int n){
+        stack<char> tempSt = st;
+
+        for(int idx = n-1; idx >= 0; idx--){
+            if(tempSt.top() != part[idx]){
+                return false;
+            }
+
+            tempSt.pop();
+        }
+
+        return true;
+    }
+
     string removeOccurrences(string s, string part) {
 
         // Brute Force
-        while(true){
-            int idx = s.find(part);
+        // while(true){
+        //     int idx = s.find(part);
 
-            if(idx == string::npos) {
-                break;
+        //     if(idx == string::npos) {
+        //         break;
+        //     }
+
+        //     s.erase(idx, part.length());
+        // }
+
+        // return s;
+
+        // Using Stack
+        stack<char> st;
+
+        int m = s.size();
+        int n = part.size();
+
+        for(int i = 0; i < m; i++){
+            st.push(s[i]);
+
+            if(st.size() >= n && check(st, part, n) == true){
+                for(int j = 0; j < n; j++){
+                    st.pop();
+                }
             }
-
-            s.erase(idx, part.length());
         }
 
-        return s;
+        string result = "";
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
     }
 };
