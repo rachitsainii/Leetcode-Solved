ec3e42ade2627fd12130340e41c044b7295ea33d
Rachit <rachit.saini@icloud.com>
2025-02-18 12:09:28 -0800

    Time: 0 ms (100%), Space: 8 MB (57.17%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-
-    bool matchesPattern(string &num, string &pattern){
-        for(int i = 0; i < pattern.length(); i++){
-            if((pattern[i] == 'I' && num[i] > num[i+1]) || 
-            (pattern[i] == 'D' && num[i] < num[i+1])){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     string smallestNumber(string pattern) {
-        // C++ utility : next_permutation in lexographical order.
+        // Using stack
+        // T. C - O(n)
+        // S.C - O(n+1)
 
-        // Brute force: Find all the permutations, check which is matching.
-
-        // T.C. - (n+1)! * n 
-        // S.C - O(n)
-
-        int n = pattern.length();
+        int n = pattern.size();
         string num = "";
-        for(int i = 1; i <= n+1; i++){
-            num.push_back(i + '0');
-        }
-
-        while(!matchesPattern(num, pattern)){
-            next_permutation(begin(num), end(num));
+        int counter = 1;
+        stack<char> st;
+        for(int i = 0; i <= n; i++){
+            st.push(counter + '0');
+            counter++;
+            if(i == n || pattern[i] == 'I'){
+                while(!st.empty()){
+                    num += st.top();
+                    st.pop();
+                }
+            }
         }
 
         return num;
